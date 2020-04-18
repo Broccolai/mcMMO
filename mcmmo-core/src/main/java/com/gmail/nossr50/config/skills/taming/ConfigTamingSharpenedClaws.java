8e0546e7d4fd6dc6fd3d0112e8cca71acbaa1557
Josh Taylor <josh@magmo.co.uk>
2020-04-18 16:15:22 +0100

    Fix taming.conf sharpened claws issues

--- mcmmo-core/src/main/java/com/gmail/nossr50/config/skills/taming/ConfigTamingSharpenedClaws.java
+++ mcmmo-core/src/main/java/com/gmail/nossr50/config/skills/taming/ConfigTamingSharpenedClaws.java
@@ -1,6 +1,7 @@
 package com.gmail.nossr50.config.skills.taming;
 
 import com.gmail.nossr50.datatypes.skills.properties.AbstractDamageProperty;
+import com.gmail.nossr50.datatypes.skills.properties.DamageProperty;
 import ninja.leaping.configurate.objectmapping.Setting;
 import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 
@@ -8,9 +9,9 @@ import ninja.leaping.configurate.objectmapping.serialize.ConfigSerializable;
 public class ConfigTamingSharpenedClaws {
 
     @Setting(value = "Bonus-Damage", comment = "The amount of bonus damage Sharpened Claws will add.")
-    private AbstractDamageProperty bonusDamage;
+    private DamageProperty bonusDamage = new AbstractDamageProperty(2.0, 2.0);
 
-    public AbstractDamageProperty getBonusDamage() {
+    public DamageProperty getBonusDamage() {
         return bonusDamage;
     }
 }
